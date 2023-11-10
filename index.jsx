b1e17f6827f931e717e4d48cbfa5ad068b1fbfd4
Rodrigo Garcia <130257752+Rodr1dan92@users.noreply.github.com>
2023-11-10 09:04:30 -0300

    redirección de indexjsx

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "../src/index.css";
+import App from "../src/App";
 import { ChakraProvider } from "@chakra-ui/react";
 
 
